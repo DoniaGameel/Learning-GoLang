7317080004dc6cae0285076df7466b3162e84523
DoniaGameel <doniagameel34@gmail.com>
2024-02-09 13:25:59 +0200

    section 2 excercise solution

--- exercise-3-starting-snapshot/profit_calculator.go
+++ exercise-3-starting-snapshot/profit_calculator.go
@@ -1,17 +1,44 @@
 package main
 
 import "fmt"
+import "io/ioutil"
+import "errors"
 
 func main() {
-	revenue := getUserInput("Revenue: ")
-	expenses := getUserInput("Expenses: ")
-	taxRate := getUserInput("Tax Rate: ")
+	revenue, err := getUserInput("Revenue: ")
+
+	if err != nil{
+		fmt.Print(err)
+		return
+	}
+	expenses, err := getUserInput("Expenses: ")
+
+	if err != nil{
+		panic(err)
+	}
+	taxRate,err := getUserInput("Tax Rate: ")
+
+	if err != nil{
+		fmt.Print(err)
+		return
+	}
 
 	ebt, profit, ratio := calculateFinancials(revenue, expenses, taxRate)
 
 	fmt.Printf("%.1f\n", ebt)
 	fmt.Printf("%.1f\n", profit)
 	fmt.Printf("%.3f\n", ratio)
+
+	EBTText := fmt.Sprint(ebt)
+	ProfitText := fmt.Sprint(profit)
+	RatioText := fmt.Sprint(ratio)
+
+	ioutil.WriteFile("balance.txt", []byte(EBTText), 0644)
+	ioutil.WriteFile("balance.txt", []byte(ProfitText), 0644)
+	ioutil.WriteFile("balance.txt", []byte(RatioText), 0644)
+
+	output := fmt.Sprint("EBT: %.1f\nProfit: %.1f\nRation: %.1f",ebt,profit,ratio)
+	ioutil.WriteFile("balance.txt", []byte(output), 0644)
 }
 
 func calculateFinancials(revenue, expenses, taxRate float64) (float64, float64, float64) {
@@ -21,9 +48,12 @@ func calculateFinancials(revenue, expenses, taxRate float64) (float64, float64, 
 	return ebt, profit, ratio
 }
 
-func getUserInput(infoText string) float64 {
+func getUserInput(infoText string) (float64, error) {
 	var userInput float64
 	fmt.Print(infoText)
 	fmt.Scan(&userInput)
-	return userInput
+	if userInput <=0 {
+		return 0, errors.New("Can't be non-positive value")
+	}
+	return userInput, nil
 }
